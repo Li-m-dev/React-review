9ea8eab1f60b74f94ecf992e5c0d70797a7103ea
Li Swafford <liswafford@yahoo.com>
2019-01-17 18:01:14 -0600

    full crud

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from '../components/Post/Post'
 
 class App extends Component {
   constructor() {
@@ -19,31 +20,75 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios
+    .get("https://practiceapi.devmountain.com/api/posts")
+    .then(response => {
+      // console.log(response);
+      this.setState({
+        posts: response.data
+      })
+    });
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios
+    .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then(response => {
+      console.log('response: ', response);
+      this.setState({
+        posts: response.data
+      })
+    })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+    .delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then(response => {
+      this.setState({
+        posts: response.data
+      })
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios
+    .post("https://practiceapi.devmountain.com/api/posts", {text})
+    .then(response => {
+      // console.log(response);
+      this.setState({
+        posts: response.data
+      })
+    });
   }
 
   render() {
     const { posts } = this.state;
-
+    const displayPosts = posts.map(post => {
+      return (
+        <Post 
+        key = {post.id}
+        id = {post.id}
+        text = {post.text}
+        date = {post.date}
+        updatePostFn = {this.updatePost}
+        deletePostFn = {this.deletePost}
+        />
+        
+      )
+    })
+    // console.log(displayPosts)
     return (
       <div className="App__parent">
         <Header />
 
         <section className="App__content">
 
-          <Compose />
+          <Compose
+          createPostFn = {this.createPost}
+          />
+          {displayPosts}
+          
           
         </section>
       </div>
